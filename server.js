5506d427ecd44e241a2cb940e065234cef8d8c44
atenger <106673675+atenger@users.noreply.github.com>
2025-02-07 21:29:14 -0500

    Basic front end added

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+// Serve static files from public directory
+app.use(express.static("public"));
+
 // Read compliments from JSON file
 const complimentsFile = path.join(__dirname, "data/compliments.json");
 const complimentsData = JSON.parse(fs.readFileSync(complimentsFile, "utf8"));
@@ -15,6 +18,16 @@ app.get("/compliment", (req, res) => {
   const randomCompliment = compliments[randomIndex];
 
   res.json({ compliment: randomCompliment });
+});
+
+// Specific route for about page
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/about.html"));
+});
+
+// Serve index.html for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(port, () => {
